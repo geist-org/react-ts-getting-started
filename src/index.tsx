b2394f95fa9479654b5e1120f9b95e3212c199b2
unix <unix.bio@gmail.com>
2021-06-27 00:12:13 +0800

    feat: upgrade sample project for scaleable compatibility

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { GeistProvider, CssBaseline } from '@geist-ui/react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+import { GeistProvider, CssBaseline } from '@geist-ui/react'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,10 +10,5 @@ ReactDOM.render(
       <App />
     </GeistProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById('root'),
+)
